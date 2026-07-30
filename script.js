718294fea586060ddaa9762799e822482dc973a8
Colt Sheesley <113475353+Xerver8694@users.noreply.github.com>
2026-07-30 11:23:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 getComputerChoice()
+
+function playRound(humanChoice, computerChoice) {
+  
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! You both chose ${humanChoice}.`);
+    return; 
+  }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++; 
+    console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+  } 
+  
+  else {
+    computerScore++; 
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  }
+
+  console.log(`Score -> Human: ${humanScore}, Computer: ${computerScore}`);
+}
