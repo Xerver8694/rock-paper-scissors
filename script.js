f49e2cf235dfea7890019fed9c42814e37a410a7
Colt Sheesley <113475353+Xerver8694@users.noreply.github.com>
2026-07-30 10:48:51 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function getPlayerChoice() {
+  let userInput = prompt("Please choose: rock, paper, or scissors");
+  return userInput.toLowerCase();
+}
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice())
+
+getComputerChoice()
